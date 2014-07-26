1a91ad90d7a4f67b1c6cf98d8156002652796209
Gary Slopsema <gslopsema@llnw.com>
2014-07-26 14:12:20 -0400

    Added orgtbl-mode to markdown-mode-hook

--- behavior/mode-setup.el
+++ behavior/mode-setup.el
@@ -36,6 +36,12 @@
 ;;
 (add-to-list 'prog-mode-hook #'linum-mode)
 
+;; markdown
+(add-hook 'markdown-mode-hook
+		  (lambda ()
+			(linum-mode 1)  			; Add line numbers
+			(orgtbl-mode 1)))			; Add beautiful ascii table support
+
 ;; paredit
 ;;
 ;; Wonderful lispy settings
