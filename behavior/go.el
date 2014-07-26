cfcc3ff3d801c9dfea71932846cbaf90926c4796
Gary Slopsema <gslopsema@llnw.com>
2014-07-26 14:37:00 -0400

    Changed garslo-go-tool-workspace to fit local needs

--- behavior/go.el
+++ behavior/go.el
@@ -2,7 +2,7 @@
   "Customization and setup for go"
   :group 'go)
 
-(defcustom garslo-go-tool-workspace "~/go/go-tools-ws"
+(defcustom garslo-go-tool-workspace "/Users/gslopsema/llnw/go"
   "Location of workspace containing go tools (like impl, oracle, etc.)"
   :type 'string
   :group 'garslo-go-customization
