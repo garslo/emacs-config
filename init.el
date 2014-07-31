14685823b478cf25eb7c98a9ed2df6b29bf3f8bc
Gary Slopsema <gslopsema@llnw.com>
2014-07-30 21:07:11 -0700

    Moved some config to a graphical-specific section, generalized a variable

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path "~/.emacs.d/custom/modes")
 
 (defvar go-tools-binary-path
-  "/Users/gslopsema/llnw/go/bin"
+  (expand-file-name "~/go/go-tools-ws")
   "Location of go tool binaries (e.g. oracle, cover, impl, etc.)")
 
 (setenv "PATH"
@@ -27,3 +27,15 @@
 ;; Added by emacs itself
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'set-goal-column 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- visuals/visuals.el
+++ visuals/visuals.el
@@ -1,15 +1,18 @@
+;; For graphical displays only...
+(when (display-graphic-p)
 ;; Best theme there is
-(load-theme 'tango-dark)
+(load-theme 'solarized-dark)
 ;; Remove the toolbar
 (tool-bar-mode -1)
 ;; Remove the scrollbar
 (scroll-bar-mode -1)
 ;; Smaller font size
 (set-face-attribute 'default nil :height 100) ; :height 100 => 10pt
+)
 ;; Remove the Emacs welcome screen
 (setq inhibit-splash-screen t)
 ;; Get faster response on keyhold
-(setq echo-keystrokes 0.1) 
+(setq echo-keystrokes 0.1)
 ;; Give me column numbers in the mode bar
 (setq column-number-mode t)
 ;; Smaller tabs
