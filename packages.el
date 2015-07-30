de8b955ff9ad575b6364a55f8b73b40566494b8c
Gary Slopsema <gslopsema@llnw.com>
2015-07-30 06:19:22 -0700

    Moved packages to top-level so they'll load before config

---
+++ packages.el
@@ -0,0 +1,59 @@
+(require 'package)
+;; ========== Package Archives ========== ;;
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(package-initialize)
+
+;; make sure to have downloaded archive description.
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+;; Auto-install these packages
+(ensure-package-installed
+ 'markdown-mode+
+ 'markdown-toc
+ 'markdown-mode
+ 'git-gutter-fringe
+ 'fringe-helper
+ 'flycheck
+ 'git-gutter
+ 'go-autocomplete
+ 'auto-complete
+ 'go-errcheck
+ 'go-play
+ 'go-projectile
+ 'go-eldoc
+ 'gotest
+ 'go-mode
+ 'helm
+ 'f
+ 'magit
+ 'paredit
+ 'popup
+ 'projectile
+ 'pkg-info
+ 'epl
+ 'dash
+ 's
+ 'dockerfile-mode
+ 'go-direx
+ 'yaml-mode
+ 'jedi
+ 'multiple-cursors
+ 'slime
+ 'haskell-mode
+ 'avy
+ 'helm-swoop
+ 'smart-mode-line
+ 'discover-my-major
+ 'yasnippet
+ 'key-chord
+ 'helm-commandlinefu
+ 'paradox
+ 'expand-region
+ 'helm-projectile
+ 'undercover
+)
+
+;(package-initialize)
