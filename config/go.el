1b7a2e18f34c037c9a6036e7738c6ce867a70fd3
Gary Slopsema <gslopsema@gmail.com>
2016-03-25 08:25:20 -0400

    compat

--- config/go.el
+++ config/go.el
@@ -2,25 +2,12 @@
   "Customization and setup for go"
   :group 'go)
 
-(defcustom garslo-go-tool-workspace (expand-file-name "/usr")
-  "Location of workspace containing go tools (like impl, oracle, etc.)"
-  :type 'string
-  :group 'garslo-go-customization
-  :safe 'stringp)
-
-(defun garslo-go--path (subdir)
-  (concat
-   garslo-go-tool-workspace
-   "/"
-   subdir))
-
 ;; gocode
-(add-to-list 'load-path (garslo-go--path "src/github.com/nsf/gocode/emacs"))
 (require 'go-autocomplete)
 (require 'auto-complete-config)
 
 ;; goimports is a better gofmt
-(setq gofmt-command (garslo-go--path "bin/goimports"))
+(setq gofmt-command "goimports")
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 ;; go-helper
@@ -31,14 +18,10 @@
 (require 'goh-mode)
 (goh-global-mode 1)
 
-;; ginkgo
-(require 'ginkgo-mode)
-
-
 ;; hook
 (add-hook 'go-mode-hook (lambda ()
                           (go-eldoc-setup)
-                          (ginkgo-mode)))
+						  ))
 
 ;;;; Keybindings
 (add-hook 'go-mode-hook
@@ -52,6 +35,4 @@
 			(local-set-key (kbd "C-c mi") 'gobb-make-interface)
 			(local-set-key (kbd "C-c mb") 'gobb-make-builder)
 			(local-set-key (kbd "C-c mm") 'gobb-make-builder-and-interface)
-			;(key-chord-define-local "oi" 'go-oracle-implements)
-			;(key-chord-define-local "or" 'go-oracle-referrers)
 			))
